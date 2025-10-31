96aee6052ddce6cd847a2778e0e6d201b33701d2
nadoman354 <52532429+nadoman354@users.noreply.github.com>
2025-10-31 16:49:20 +0900

    zz

--- Assets/Scripts/LeeJunmo/Item_SO.cs
+++ Assets/Scripts/LeeJunmo/Item_SO.cs
@@ -1,16 +1,17 @@
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
-public class Item_SO : MonoBehaviour
+[CreateAssetMenu(fileName = "Item", menuName = "Game/Items")]
+public class Item_SO : ScriptableObject
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("아이템 이름")]
+    public string itemName;
+    [Header("태그")]
+    public List<ItemTag> tags;
+    [Header("업그레이드 최대 횟수")]
+    public int MaxUpgrade;
+    [Header("아이템 설명")]
+    public string itemScript;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
