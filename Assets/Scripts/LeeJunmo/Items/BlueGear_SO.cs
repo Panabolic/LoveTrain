bdbc8933b7c7b4de9fbae44c7e754d7071b29fca
nadoman354 <nadoman354@naver.com>
2025-11-21 15:08:02 +0900

    zz

---
+++ Assets/Scripts/LeeJunmo/Items/BlueGear_SO.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BlueGear", menuName = "Items/BlueGear")]
+public class BlueGear_SO : Item_SO
+{
+    [Header("리볼버 전용 데이터")]
+    public float[] AttackSpeedByLevel = { 10f, 20f, 30f };
+
+    public override GameObject OnEquip(GameObject user, ItemInstance instance)
+    {
+        // 1. 부모의 공통 함수를 호출해 '로직+시각' 프리팹 생성
+        GameObject BlueGearGO = InstantiateVisual(user);
+        if (BlueGearGO == null) return null;
+        
+
+        return BlueGearGO;
+    }
+
+    protected override Dictionary<string, string> GetStatReplacements(int level)
+    {
+        int index = Mathf.Clamp(level - 1, 0, AttackSpeedByLevel.Length - 1);
+
+        return new Dictionary<string, string>
+        {
+            { "AttackSpeed", AttackSpeedByLevel[index].ToString() },
+        };
+    }
+}
---
+++ Assets/Scripts/LeeJunmo/Items/RedGear_SO.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RedGear", menuName = "Items/RedGear")]
+public class RedGear_SO : Item_SO
+{
+    [Header("리볼버 전용 데이터")]
+    public float[] DamageByLevel = { 10f, 20f, 30f };
+
+    public override GameObject OnEquip(GameObject user, ItemInstance instance)
+    {
+        // 1. 부모의 공통 함수를 호출해 '로직+시각' 프리팹 생성
+        GameObject RedGear = InstantiateVisual(user);
+        if (RedGear == null) return null;
+        
+
+        return RedGear;
+    }
+
+    protected override Dictionary<string, string> GetStatReplacements(int level)
+    {
+        int index = Mathf.Clamp(level - 1, 0, DamageByLevel.Length - 1);
+
+        return new Dictionary<string, string>
+        {
+            { "Damage", DamageByLevel[index].ToString() },
+        };
+    }
+}
